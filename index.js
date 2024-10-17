2436747d735a3164a6e9a4379ec2f3bc8ea74af7
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-17 13:06:09 +0530

    add

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.set('view engine','ejs');
+app.set("view engine","ejs");
+app.set('views', path.join(directoryPath,'views'));
 app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     fs.readdir(`./files`,(err,file)=>{
-        res.render("index.ejs",{Files:file});
+        res.render("index",{Files:file});
     });
 })
 app.get("/files/:fileName",(req,res)=>{
