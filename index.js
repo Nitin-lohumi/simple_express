1bbb7c739cc2cbb6aa2eb35a7f8e44ca7d72c662
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-17 13:13:57 +0530

    add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
-app.set('views', path.join(__dirname,'views'));
+// app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     fs.readdir(`./files`,(err,file)=>{
